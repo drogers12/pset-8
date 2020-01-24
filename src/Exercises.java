ef3dbaaa93248bdb25739bfaf64a48934e739c17
drogers12 <54987779+drogers12@users.noreply.github.com>
2020-01-24 01:49:09 -0500

    Clumps finished

--- src/Exercises.java
+++ src/Exercises.java
@@ -4,7 +4,7 @@ public class Exercises {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
-		
+
 		// write your code here
 
 		if (a[a.length-1] == b[b.length-1] || a[0] == b[0]) {
@@ -12,50 +12,50 @@ public class Exercises {
 		}
 		return false;
 	}
-	
+
 	public String[] endsMeet(String[] values, int n) {
 		if (values == null || values.length < n || n < 1) {
 			String[] empty = new String[0];
 			return empty;
-			
+
 		} else {
-			
+
 			String[] newArray = new String[n*2];
 			int x = n;
 			for(int i = 0; i < n; i++) {
 				newArray[i] = values[i];
 			}
-			
+
 			for(int i = values.length-n; i < values.length; i++) {
 				newArray[x] = values[i];
 				x++;
 			}
-			
+
 			return newArray;
-			
+
 		}
 	}
-	
+
 	public int difference(int[] numbers) {
-		
+
 		if (numbers == null || numbers.length < 1) {
-			return -1;	
+			return -1;
 		} else {
 			int max = numbers[0];
 			int min = numbers[0];
-			
+
 			for (int i = 1; i < numbers.length; i++) {
 	             if (numbers[i] > max) {
-	            	 max = numbers[i];  
+	            	 max = numbers[i];
 	             }
 	             if (numbers[i] < min) {
-	            	 min = numbers[i];  
+	            	 min = numbers[i];
 	             }
 			}
 			return max-min;
 		}
 	}
-	
+
 	public double biggest(double[] numbers) {
 		if (numbers == null || numbers.length < 3 || numbers.length % 2 == 0) {
 			return -1;
@@ -80,7 +80,7 @@ public class Exercises {
 		}
 		return -1; //default value
 	}
-	
+
 	public String[] middle(String[] values) {
 		String[] empty = new String[0];
 		if (values == null || values.length < 3 || values.length % 2 == 0) {
@@ -115,7 +115,7 @@ public class Exercises {
 			return false;
 		}
 	}
-	
+
 	public boolean everywhere(int[] numbers, int x) {
 		if (numbers == null || numbers.length < 1) {
 			return false;
@@ -133,22 +133,62 @@ public class Exercises {
 			}
 		}
 	}
-	
-	public boolean consecutive(int[] numbers) {
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
+
+  public boolean consecutive(int[] numbers) {
+		if (numbers == null || numbers.length < 3) {
+			return false;
+		} else {
+			int correct = 0;
+			for(int i = 0; i < numbers.length-2; i++) {
+				if((numbers[i] % 2 == 0 && numbers[i+1] % 2 == 0 && numbers[i+2] % 2 == 0) || numbers[i] % 2 == 1 && numbers[i+1] % 2 == 1 && numbers[i+2] % 2 == 1) {
+					correct += 1;
+				}
+			}
+			if(correct > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
-	
-	public boolean balance(int[] numbers) {
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
+
+
+  public boolean balance(int[] numbers) {
+		if (numbers == null || numbers.length < 2) {
+			return false;
+		} else {
+			int left = 0;
+			int right = 0;
+			for(int x = 0; x < numbers.length; x++) {
+				right += numbers[x];
+			}
+			for(int i = 0; i < numbers.length-1; i++) {
+				if(left != right) {
+					left += numbers[i];
+					right -= numbers[i];
+			    }
+			}
+		return left==right;
+		}
 	}
-	
-	public int clumps(String[] values) {
-		// write your code here
-		
-		return -1;		// default return value to ensure compilation
+
+  public int clumps(String[] values) {
+		if (values == null) {
+			return -1;
+		} else {
+			int clumps = 0;
+			boolean isClump = false;
+		  for(int i = 0; i < values.length - 1; i++) {
+			  if (isClump) {
+				  if(!values[i].equals(values[i+1])) {
+					  isClump = false;
+				  }
+			  } else if (values[i].equals(values[i+1])) {
+				  isClump = true;
+				  clumps++;
+			  }
+		  }
+		  return clumps;
+		}
 	}
 }
